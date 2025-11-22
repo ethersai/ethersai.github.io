b1cb821067ef6603e0b450374fa87c35f1a385c6
mdeter777 <eter_wheyy@proton.me>
2025-11-22 13:56:10 +0100

    Commit

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-// Select the element by its ID
-const button = document.getElementById("clickMe");
+let button = document.getElementById("clickMe");
+let heading = document.querySelector("h1");
 
-// Add a click event listener
-button.addEventListener("click", function() {
-    alert("You clicked me!");
+
+heading.addEventListener("click", function() {
+    heading.textContent = "You clicked me!";
 });
+
+// Select the element by its ID
+// const button = document.getElementById("clickMe");
+
+// // Add a click event listener
+// button.addEventListener("click", function() {
+//     alert("You clicked me!");
+// });
+
+
+// heading.addEventListener("click", function() {
+//     heading.textContent = "You clicked me!";
+// });
