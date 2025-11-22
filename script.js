a582ff08f09916cdf4c3a34de65d8717113d6a83
mdeter777 <eter_wheyy@proton.me>
2025-11-22 13:51:36 +0100

    Commit

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("JS is working");
+// Select the element by its ID
+const button = document.getElementById("clickMe");
+
+// Add a click event listener
+button.addEventListener("click", function() {
+    alert("You clicked me!");
+});
